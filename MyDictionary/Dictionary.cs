b78e328f0ce2a4a978ef09966ed1d9acc84e86c3
Zehra GÖKSU <edaxzehra@hotmail.com>
2021-03-08 11:34:02 +0300

    Add project files.

---
+++ MyDictionary/Dictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+	class Dictionary<T,V>
+	{
+		T[] keys;
+		V[] values;
+
+		public Dictionary()
+		{
+			keys = new T[0];
+			values = new V[0];
+		}
+
+		public void Add(T key, V value)
+		{
+			T[] tempkey = keys;
+			V[] tempvalue = values;
+			keys = new T[keys.Length+1];
+			values = new V[values.Length + 1];
+			
+
+			for (int i = 0; i < tempkey.Length; i++)
+			{
+				keys[i] = tempkey[i];
+				values[i] = tempvalue[i];
+			}
+
+			keys[keys.Length - 1] = key;
+			values[values.Length - 1] = value;
+		}
+
+		public int Length { get { return keys.Length; } }
+
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Dictionary<int, string> customerNo = new Dictionary<int, string>();
+			customerNo.Add(1, "Tuncay");
+			customerNo.Add(2, "Olgun");
+			customerNo.Add(3, "Taner");
+			Console.WriteLine(customerNo.Length);
+
+		}
+	}
+}
